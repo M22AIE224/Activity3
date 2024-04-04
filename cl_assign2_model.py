96ed194bf5fe0cc13e4eba24f675f79953c459d2
Sharma <Prabha_Sharma@DELL.com>
2024-04-04 22:59:03 +0530

    Version2

--- cl_assign2_model.py
+++ cl_assign2_model.py
@@ -10,6 +10,7 @@ if(torch.cuda.is_available()):
 else:
   device = "cpu"
 
+print("Version2 Changes")
 def plot_training_curves(train_loss_history, train_accuracy_history, optimizer_name):
     plt.plot(range(1, 10 + 1), train_loss_history, label='Training Loss')
     plt.plot(range(1, 10 + 1), train_accuracy_history, label='Training Accuracy')
